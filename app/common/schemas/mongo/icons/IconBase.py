5abf09a72ecb0e968c3845139e63d3ee40365c5e
Eldad Berger <eldadberger@gmail.com>
2025-09-19 15:33:45 +0300

    add

--- app/common/schemas/mongo/icons/IconBase.py
+++ app/common/schemas/mongo/icons/IconBase.py
@@ -1,6 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+from app.common.schemas.mongo.icons.MutationWeight import MutationWeight
 
 
 class IconBase(BaseModel):
     name: str
     svg: str
+    rotation_symmetry_angle: int | None = Field(None, alias="rotationSymmetryAngle")
+    mutation_weight: MutationWeight | None = Field(None, alias="mutationWeight")
